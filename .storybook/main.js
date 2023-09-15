35156f6e09d75287916580c7e9f326013d332e96
Matthew Buckett <matthew.buckett@ctl.ox.ac.uk>
2023-09-15 13:20:14 +0100

    Upgrade the version of storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,8 +3,22 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+
+  docs: {
+    autodocs: true
+  },
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+
+  features: {
+    storyStoreV7: false,
+  },
 }
