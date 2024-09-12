293f9668f6cc8644001f6e5976be01ba5731dfb9
Matthew Buckett <matthew.buckett@ctl.ox.ac.uk>
2024-09-12 17:42:56 +0100

    Add PromptOAuth component
    This allows for the UI of a tool to continue to be shown, but for a prompt to grant access to be overlaid on-top.
    
    This was added as a new component as it's behaviour is quite different to the existing OAuth component in that it doesn't have any child components.

--- src/index.js
+++ src/index.js
@@ -6,3 +6,4 @@ export * from './components/tokenRetriever/LtiTokenRetriever';
 export * from './components/errorBillboard/ErrorBillboard';
 export * from './components/launchOAuth/LaunchOAuth';
 export * from './components/applyTheme/LtiApplyTheme';
+export * from './components/promptOAuth/PromptOAuth';
