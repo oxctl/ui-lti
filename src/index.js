721514687471f12e49f46e6b8b3b038837b979b1
Matthew Buckett <matthew.buckett@ctl.ox.ac.uk>
2025-06-10 15:42:32 +0100

    Export the new component.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ export * from './components/heightLimit/LtiHeightLimit.jsx';
 export * from './components/modal/LtiLimitModal.jsx';
 export * from './components/tokenRetriever/LtiTokenRetriever.jsx';
 export * from './components/errorBillboard/ErrorBillboard.jsx';
+export * from './components/LtiPageSettings/LtiPageSettings.jsx';
 export * from './components/launchOAuth/LaunchOAuth.jsx';
 export * from './components/applyTheme/LtiApplyTheme.jsx';
 export * from './components/promptOAuth/PromptOAuth.jsx';
